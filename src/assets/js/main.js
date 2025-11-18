a79910bc2a9a671f997192e57fc75bd79b77d359
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2025-11-18 19:32:01 +0100

    Add one more image to carousel and set looping mode

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,7 +9,7 @@ import EmblaCarousel from 'embla-carousel';
 import Autoplay from 'embla-carousel-autoplay';
 
 const emblaNode = document.querySelector('.embla');
-const options = { loop: false };
+const options = { loop: true };
 const plugins = [Autoplay()];
 const emblaApi = EmblaCarousel(emblaNode, options, plugins);
 
