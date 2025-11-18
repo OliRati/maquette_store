7f7e789899d5b8642ac6a988c0d08f29047c271a
Olivier RATIEVILLE <olivier.ratieville@gmail.com>
2025-11-18 16:02:20 +0100

    First batch : Initialize and start the job

---
+++ src/assets/js/main.js
@@ -0,0 +1,57 @@
+import '../css/style.css'
+import { menuContent } from '../js/menuContent.js'
+
+const menu = document.querySelector('.menu');
+
+const ul = document.createElement('ul');
+
+menuContent.forEach((item) => {
+  const li = document.createElement('li');
+  li.classList.add('menuItem');
+  li.innerText = item.title;
+
+  if (item.subMenu.length > 0) {
+    const innerDiv = document.createElement('div');
+    innerDiv.classList.add('submenuItem');
+
+    const innerUl = document.createElement('ul');
+    item.subMenu.forEach((item) => {
+      const innerLi = document.createElement('li');
+      innerLi.innerText = item.title;
+      innerUl.append(innerLi);
+    });
+
+    innerDiv.append(innerUl);
+    li.append(innerDiv);
+  };
+
+  ul.append(li);
+});
+
+menu.append(ul);
+
+/*
+ * Hamburger Menu switch
+ */
+
+const menuContainer = document.querySelector('.menuContainer');
+
+const hamburger = document.getElementById('hamburger');
+let toggleHamburger = false;
+hamburger.addEventListener('click', () => {
+  toggleHamburger = !toggleHamburger;
+
+  const i = hamburger.firstElementChild;
+  if (toggleHamburger) {
+    i.classList.remove('fa-bars');
+    i.classList.add('fa-xmark');
+    menu.classList.add('showMenu');
+    menuContainer.classList.add('menuContainerFullscreen');
+  } else {
+    i.classList.add('fa-bars');
+    i.classList.remove('fa-xmark');
+    menu.classList.remove('showMenu');
+    menuContainer.classList.remove('menuContainerFullscreen');
+  }
+
+});
